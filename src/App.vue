ade10da578264e351881de89f5779f34091bcd6c
Ann Kilzer <akilzer@gmail.com>
2019-07-06 14:56:44 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/melon400.png">
+    <MelonCoin msg="THE FUTURE IS FRUIT TOKENS"/>
+  </div>
+</template>
+
+<script>
+import MelonCoin from './components/MelonCoin.vue'
+
+export default {
+  name: 'app',
+  components: {
+    MelonCoin
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto');
+#app {
+  font-family: 'Roboto', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MelonCoin.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="melon">
+    <h1>{{ msg }}</h1>
+    <p>
+      In the beginning, there were melons. And over 150 years since the beginning of the capitalist society,
+      everything changed. Through the power of blockchain, the humble farmers of Yubari, Hokkaido, created a
+      revolutionary payment system backed by the world's most expensive fruit.
+      It's not just a cryptocurrency -- it's a state of mind.
+    </p>
+    <p>
+      メロンコインはフルーツの仮想通貨です。
+    </p>
+    <h3>Essential Links</h3>
+    <ul>
+      <li><a href="https://twitter.com/meloncointoken" target="_blank">Twitter</a></li>
+      <li><a href="assets/meloncoin-whitepaper-v1.pdf" target="_blank">Whitepaper</a></li>
+      <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSd8O1BB_VzD9ymLZ1rhUBT6ZE7YhIrZWnGrtwLf-rbTBqXfJg/viewform?usp=sf_link" target="_blank">Mailing List</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MelonCoin',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
